6847a2470fc577c4c96d81ef11b1255bb200a4f4
Shawn Strickland <sjstrick@me.com>
2022-10-28 18:02:44 -0500

    add working PUT/GET s3 actions from api gateway copy to -shawn.tf files in case of merge conflict

---
+++ variables-shawn.tf
@@ -0,0 +1,11 @@
+variable "tag_name" {
+  description = "Value of the Name tag for all resources"
+  type        = string
+  default     = "Team4"
+}
+
+variable "region" {
+  description = "Region for Cloud Challenge Apps"
+  type        = string
+  default     = "us-east-1"
+}
